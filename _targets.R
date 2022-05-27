986e498d7a3203d808fb8f929f95807adf879fbd
Julia Ortmann <58432678+juleZWK@users.noreply.github.com>
2022-05-28 00:18:51 +0200

    targets - AnalysisWorkflow

--- _targets.R
+++ _targets.R
@@ -1,6 +1,10 @@
 library(targets)
 options(crayon.enabled = FALSE)
 tar_option_set(memory = "transient", garbage_collection = TRUE)
+tar_option_set(packages = c("RJobTissueArea", "tibble", "readr", 
+    "dplyr", "ggplot2"), imports = c("RJobTissueArea"))
+Rfiles <- list.files("R")
+purrr::walk(Rfiles, ~source(file.path("R", .x)))
 create_plot <- function(data) {
     ggplot(data) + geom_histogram(aes(x = Ozone), bins = 12) + 
         theme_gray(24)
